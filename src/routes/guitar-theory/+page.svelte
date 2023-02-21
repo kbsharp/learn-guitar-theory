c2980019f4682c6e728d10a8f2c1a1b0dca8e8d5
Kevin Bevan <kbevan.dev@gmail.com>
2023-02-21 00:13:08 +0000

    batman 🦇

---
+++ src/routes/guitar-theory/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Fretboard from './Fretboard.svelte';
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Guitar Theory</h1>
+
+	<Fretboard />
+</div>
---
+++ src/routes/guitar-theory/Fretboard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let frets = new Array(22).fill(null);
+</script>
+
+<div class="fretboard">
+	{#each frets as fret}
+		<div class="fret">#</div>
+	{/each}
+</div>
+
+<style lang="scss">
+	.fretboard {
+		display: flex;
+		background-color: rgba(0, 0, 0, 0.137);
+		width: 100%;
+		height: 100%;
+
+		.fret {
+			height: 200px;
+			width: 120px;
+			color: white;
+		}
+	}
+</style>
